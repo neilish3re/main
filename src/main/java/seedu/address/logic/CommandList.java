3db5468653bdbbfd3cd380f933e89ced5f385770
neilish3re <neiljm007@yahoo.com.sg>
2018-11-01 22:00:41 +0800

    Utility Classes for Autocomplete Feature

---
+++ src/main/java/seedu/address/logic/CommandList.java
@@ -0,0 +1,31 @@
+package seedu.address.logic;
+
+import seedu.address.logic.commands.AddCommand;
+import seedu.address.logic.commands.CheckoutCommand;
+import seedu.address.logic.commands.ClearCommand;
+
+import seedu.address.logic.commands.DeleteCommand;
+import seedu.address.logic.commands.EditCommand;
+import seedu.address.logic.commands.ExitCommand;
+
+import seedu.address.logic.commands.FindCommand;
+import seedu.address.logic.commands.HelpCommand;
+import seedu.address.logic.commands.HistoryCommand;
+
+import seedu.address.logic.commands.ListCommand;
+import seedu.address.logic.commands.SelectCommand;
+import seedu.address.logic.commands.UndoCommand;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Stores list of all available commands
+ */
+public class CommandList {
+    public static final List<String> COMMAND_LIST = Arrays.asList(AddCommand.COMMAND_WORD,
+            CheckoutCommand.COMMAND_WORD, ClearCommand.COMMAND_WORD, DeleteCommand.COMMAND_WORD,
+            EditCommand.COMMAND_WORD, ExitCommand.COMMAND_WORD, FindCommand.COMMAND_WORD,
+            HelpCommand.COMMAND_WORD, HistoryCommand.COMMAND_WORD, ListCommand.COMMAND_WORD,
+            SelectCommand.COMMAND_WORD, UndoCommand.COMMAND_WORD);
+}
